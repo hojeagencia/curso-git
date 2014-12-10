768be6194708dbaea880f117488a79b052e983dd
Diego <diego@hojeagencia.com.br>
2014-12-10 18:53:49 -0200

    Criada a view

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso git - compartilhado</title>
+</head>
+<body>
+	<h1>Curso git - Compartilhado</h1>
+</body>
+</html>
