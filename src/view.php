b59c9254f95479558eb87c2b0acda7045f6aba1a
Diego <diego@hojeagencia.com.br>
2014-12-11 19:15:04 -0200

    inclusão de novo css e correção de caminho

--- src/view.php
+++ src/view.php
@@ -7,7 +7,8 @@
     <title>Bootstrap 101 Template</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="src/css/bootstrap.css">
+    <link rel="stylesheet" href="src/css/style.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
